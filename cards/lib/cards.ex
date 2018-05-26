6fba9133bd9f639ad367cbbd847f756fb64fb744
Josué Vélez <josuevelezdr@gmail.com>
2018-05-26 10:54:29 -0500

    pattern matching, erlang lenguage

--- cards/lib/cards.ex
+++ cards/lib/cards.ex
@@ -1,11 +1,37 @@
 defmodule Cards do
 
+  # check this out https://hexdocs.pm/elixir/Kernel.html
+
   def create_deck do
-    ['ACE', 'TWO', 'THREE']
+    values =  ['ACE', 'TWO', 'THREE', 'FOUR', 'FIVE']
+    suits = ['Spades', 'Clubs', 'Hearts', 'Diamonds']
+
+    for suit <- suits, value <- values do
+      "#{value} of #{suit}"
+    end
   end
 
   def shuffle(deck) do
     Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+  def deal(deck, hand_size) do
+    # pattern matching ---
+    {hand, rest} = Enum.split(deck, hand_size)
+    IO.puts 'hand pick up'
+    IO.puts hand
+    IO.puts 'rest of deck'
+    IO.puts rest
+    {hand, rest}
+  end
+
+  def save(deck, file_name) do
+    binary = :erlang.term_to_binary(deck) #Usign erlang programming language
+    File.write(file_name, binary)
   end
 
 end
